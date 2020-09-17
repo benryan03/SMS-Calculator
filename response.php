9ff6a55f5cb0d35c86981f8ee7c201cbc216364c
Ben Ryan <benryan03@gmail.com>
2020-09-17 19:50:58 -0400

    First working version

--- response.php
+++ response.php
@@ -1,73 +1,76 @@
 <?php
 
-    use Twilio\Twiml\MessagingResponse;
+use Twilio\Twiml\MessagingResponse;
 
-    // start the session
-    session_start();
+// start the session
+session_start();
 
-    // Check session variables
-    $firstNum = null;
-    $secondNum = null;
-    if(isset($_SESSION['firstNum'])){
-        $firstNum = $_SESSION['firstNum'];
+// Check session variables
+$firstNum = null;
+$secondNum = null;
+if(isset($_SESSION['firstNum'])){
+    $firstNum = $_SESSION['firstNum'];
+}
+if(isset($_SESSION['secondNum'])){
+    $secondNum = $_SESSION['secondNum'];
+}
+
+
+$input = $_REQUEST['Body'];
+
+// CLEAR
+if ($input == "Clear"){
+    session_unset();
+    $firstNum = null; // DEBUG
+    $secondNum = null; // DEBUG
+    $operation = null; // DEBUG
+    $output = "Cleared.";
+}
+
+// OPERATION OR NUMBER
+else { // OPERATION
+    if(!isset($_SESSION['operation'])) {
+        $operation = $input;
+        $output = $operation . ": Enter the first number.";
     }
-    if(isset($_SESSION['secondNum'])){
-        $secondNum = $_SESSION['secondNum'];
-    }
+    else { // NUMBER
+        $operation = $_SESSION['operation'];
+        if ($firstNum == null) { // FIRST NUMBER
+            $firstNum = $_REQUEST['Body'];
+            $output = "Enter the second number.";}
 
+        elseif($firstNum != null & $secondNum == null) { // SECOND NUMBER
+            $secondNum = $_REQUEST['Body'];
 
-
-
-
-
-    $input = $_REQUEST['Body'];
-
-    // CLEAR
-    if ($input == "Clear"){
-        session_unset();
-        $output = "Cleared.";
-    }
-
-    // OPERATION OR NUMBER
-    else { // OPERATION
-        if(!isset($_SESSION['operation'])) {
-            $operation = $input;
-            $output = $operation . ": Enter the first number.";
-        }
-        else { // NUMBER
-            $operation = $_SESSION['operation'];
-            if ($firstNum == null) { // FIRST NUMBER
-                $firstNum = $_REQUEST['Body'];
-                $output = "Enter the second number.";}
-
-            elseif($firstNum != null & $secondNum == null) { // SECOND NUMBER
-                $secondNum = $_REQUEST['Body'];
-
-                // CALCULATE RESULT
-                $output = $firstNum . $operation . $secondNum;
-                session_unset();
+            // CALCULATE RESULT
+            if ($operation == "+"){
+                $result = $firstNum + $secondNum;
+            }
+            elseif ($operation == "-"){
+                $result = $firstNum - $secondNum;
+            }
+            elseif ($operation == "*"){
+                $result = $firstNum * $secondNum;
+            }
+            elseif ($operation == "/"){
+                $result = $firstNum / $secondNum;
             }
 
-            
+            $output = $firstNum . " " . $operation . " " . $secondNum . " = " . $result;
+            session_unset();
         }
 
-        // Save session variables
-        $_SESSION['operation'] = $operation;
-        $_SESSION['firstNum'] = $firstNum;
-        $_SESSION['secondNum'] = $secondNum;
+        
     }
 
-    
-    $output = $operation . "-" . $firstNum . "-" . $secondNum . "---" . $output; // DEBUG
+    // Save session variables
+    $_SESSION['operation'] = $operation;
+    $_SESSION['firstNum'] = $firstNum;
+    $_SESSION['secondNum'] = $secondNum;
+}
 
-
-
-
-
-
-
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 ?>
 <Response>
