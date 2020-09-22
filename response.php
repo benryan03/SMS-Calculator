3cb9283a9d51c7e8577ce5755c934217acf13088
Ben Ryan <benryan03@gmail.com>
2020-09-21 23:49:51 -0400

    Change order of inputs

--- response.php
+++ response.php
@@ -7,17 +7,15 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 session_start();
 
-function saveSessionVariables(){
-    $_SESSION['operation'] = $operation;
-    $_SESSION['firstNum'] = $firstNum;
-    $_SESSION['secondNum'] = $secondNum;
-}
-
 // Import session variables if they exist
 $firstNum = null;
+$operation = null;
 $secondNum = null;
 if(isset($_SESSION['firstNum'])){
     $firstNum = $_SESSION['firstNum'];
+}
+if(isset($_SESSION['operation'])){
+    $operation = $_SESSION['operation'];
 }
 if(isset($_SESSION['secondNum'])){
     $secondNum = $_SESSION['secondNum'];
@@ -29,41 +27,65 @@ $input = $_REQUEST['Body'];
 // CLEAR
 if (strtolower($input) == "clear" | strtolower($input) == "c"){
     session_unset();
+    $firstNum = null; // DEBUG
+    $operation = null; // DEBUG
+    $secondNum = null; // DEBUG
     $output = "Cleared.";
 }
 
-// OPERATION
-elseif (!isset($_SESSION['operation'])){
-    $operation = $input;
-    $output = $operation . ": Enter the first number.";
-    saveSessionVariables();
-}
-
 // FIRST NUMBER
-elseif ($firstNum == null) {
-    $operation = $_SESSION['operation'];
-    $firstNum = $_REQUEST['Body'];
-    $output = "Enter the second number.";
-    saveSessionVariables();
+elseif ($firstNum == null){
+    if (is_numeric($input)){
+        $firstNum = $input;
+        $output = "Enter the operation: ";
+    }
+    else {
+        $output = "Error: Enter a number.";
+    }
 }
 
-// SECOND NUMBER - CALCULATE RESULT
-elseif($firstNum != null & $secondNum == null) {
-    $operation = $_SESSION['operation'];
-    $secondNum = $_REQUEST['Body'];
-    switch($operation){
-        case "+":
-            $result = $firstNum + $secondNum;
-        case "-":
-            $result = $firstNum - $secondNum;
-        case "*":
-            $result = $firstNum * $secondNum;
-        case "/":
-            $result = $firstNum / $secondNum;
+// OPERATION
+elseif ($operation == null){
+    if ($input == "+" | $input == "-" | $input == "*" | $input == "/"){
+        $operation = $input;
+        $output = "Enter the second number: ";
     }
-    $output = $firstNum . " " . $operation . " " . $secondNum . " = " . $result;
-    session_unset();
+    else {
+        $output = "Error: Supported operations are +, -, *, or /";
+    }
 }
+
+// SECOND NUMBER
+elseif ($secondNum == null){
+    if (is_numeric($input)){
+        $secondNum = $input;
+
+        // CALCULATE RESULT
+        if ($operation == "+"){
+            $result = $firstNum + $secondNum;
+        }
+        elseif ($operation == "-"){
+            $result = $firstNum - $secondNum;
+        }
+        elseif ($operation == "*"){
+            $result = $firstNum * $secondNum;
+        }
+        elseif ($operation == "/"){
+            $result = $firstNum / $secondNum;
+        }
+        
+        $output = $firstNum . " " . $operation . " " . $secondNum . " = " . $result;
+        session_unset();
+    }
+    else {
+        $output = "Error: Enter a number.";
+    }
+}
+
+// Save session variables
+$_SESSION['firstNum'] = $firstNum;
+$_SESSION['operation'] = $operation;
+$_SESSION['secondNum'] = $secondNum;
 
 ?>
 
